f51db61cb5a1d9f0568bcaaccd4fa2696318ed39
nipunaRajapaksha <nshalitha7@gmail.com>
2024-04-13 13:52:04 +0200

    remove unwanted tests

--- apps/sudoku-app-game/src/app/app.component.spec.ts
+++ apps/sudoku-app-game/src/app/app.component.spec.ts
@@ -9,19 +9,4 @@ describe('AppComponent', () => {
       imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
     }).compileComponents();
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome sudoku-app-game'
-    );
-  });
-
-  it(`should have as title 'sudoku-app-game'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('sudoku-app-game');
-  });
 });
