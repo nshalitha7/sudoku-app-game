3f8a0e03694137bcfd8a6a2627c61d1d0a1a711d
nipunaRajapaksha <nshalitha7@gmail.com>
2024-04-15 01:55:53 +0200

    setup unit test report

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,15 @@ import { getJestProjectsAsync } from '@nx/jest';
 
 export default async () => ({
   projects: await getJestProjectsAsync(),
+  collectCoverage: true,
+  coverageReporters: ['html', 'lcov', 'text-summary'],
+  coverageDirectory: '<rootDir>/coverage/',
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
 });
