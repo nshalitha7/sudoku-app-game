cbfd94176c05c19a98cfb64af92520d607de64ca
nipunaRajapaksha <nshalitha7@gmail.com>
2024-04-14 18:42:08 +0200

    multiplayer service

--- libs/sudoku-web/sudoku-logic/src/lib/sudoku-logic/sudoku-logic.service.ts
+++ libs/sudoku-web/sudoku-logic/src/lib/sudoku-logic/sudoku-logic.service.ts
@@ -1,32 +1,52 @@
 import { GameApiSudoku } from '@sudoku-app-game/game-api';
-import { GameDifficulty, GameStatus } from '@sudoku-app-game/sudoku-models';
+import {
+  GameDifficulty,
+  GameStatus,
+  SudokuCell,
+  CellType,
+} from '@sudoku-app-game/sudoku-models';
 import { BehaviorSubject, firstValueFrom } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { SudokuMultiplayer } from './sudoku-multiplayer.service';
 
 @Injectable({ providedIn: 'root' })
 export class SudokuLogic {
-  board$: BehaviorSubject<number[][]>;
+  board$: BehaviorSubject<SudokuCell[][]>;
   status$: BehaviorSubject<GameStatus>;
   difficulty$: BehaviorSubject<GameDifficulty>;
-  private board: number[][];
+  private board: SudokuCell[][];
   private status: GameStatus;
   private difficulty: GameDifficulty;
 
-  constructor(private readonly apiSudoku: GameApiSudoku) {
+  constructor(
+    private readonly apiSudoku: GameApiSudoku,
+    private readonly sudokuMultiplayer: SudokuMultiplayer
+  ) {
     this.board = this.initializeBoard();
     this.board$ = new BehaviorSubject(this.board);
     this.status = 'unsolved';
     this.status$ = new BehaviorSubject<GameStatus>(this.status);
     this.difficulty = 'random';
     this.difficulty$ = new BehaviorSubject<GameDifficulty>(this.difficulty);
+
+    this.sudokuMultiplayer.initBoard$.subscribe(
+      (boardCoop: SudokuCell[][] | null) => {
+        if (!boardCoop) return;
+        this.board = boardCoop;
+        this.board$.next(this.board);
+      }
+    );
   }
 
-  initializeBoard(): number[][] {
-    const board: number[][] = [];
+  initializeBoard(): SudokuCell[][] {
+    const board: SudokuCell[][] = [];
     for (let i = 0; i < 9; i++) {
       board.push([]);
       for (let j = 0; j < 9; j++) {
-        board[i].push(0);
+        board[i].push({
+          type: CellType.INPUT,
+          value: 0,
+        });
       }
     }
 
@@ -46,13 +66,21 @@ export class SudokuLogic {
       throw new Error('Value must be between 0 and 9');
     }
 
-    this.board[x][y] = value;
+    this.sudokuMultiplayer.emitUpdateBoard({ x, y, value });
+    this.board[x][y] = { value, type: CellType.INPUT };
   }
 
   async setNewBoard(difficulty: GameDifficulty) {
     const { board } = await firstValueFrom(this.apiSudoku.getBoard(difficulty));
-    this.board = board;
-    this.board$.next(board);
+    this.board = board.map((col) =>
+      col.map((value): SudokuCell => {
+        return {
+          value: value,
+          type: value === 0 ? CellType.INPUT : CellType.FIXED,
+        };
+      })
+    );
+    this.board$.next(this.board);
     this.difficulty = difficulty;
     this.difficulty$.next(this.difficulty);
     this.status = 'unsolved';
@@ -61,9 +89,24 @@ export class SudokuLogic {
 
   async solveBoard() {
     const { solution, status, difficulty } = await firstValueFrom(
-      this.apiSudoku.solveBoard({ board: this.board })
+      this.apiSudoku.solveBoard({
+        board: this.board.map((col) =>
+          col.map((value) => (value.type === CellType.FIXED ? value.value : 0))
+        ),
+      })
     );
-    this.board = solution;
+    this.board = solution.map((col, colIdx) =>
+      col.map(
+        (value, rowIdx): SudokuCell => ({
+          value,
+          type:
+            this.board[colIdx][rowIdx].type === CellType.INPUT
+              ? CellType.GENERATED
+              : CellType.FIXED,
+        })
+      )
+    );
+
     this.board$.next(this.board);
     this.difficulty = difficulty;
     this.difficulty$.next(this.difficulty);
@@ -73,11 +116,15 @@ export class SudokuLogic {
 
   async validateBoard() {
     const { status } = await firstValueFrom(
-      this.apiSudoku.validateBoard({ board: this.board })
+      this.apiSudoku.validateBoard({
+        board: this.board.map((col) => col.map((value) => value.value)),
+      })
     );
     this.status = status;
-    console.log(this.board);
-    console.log('VALIDATING..', status);
     this.status$.next(this.status);
+  }
+
+  initMultiplayer(roomId: string) {
+    this.sudokuMultiplayer.init(roomId, this.board);
   }
 }
---
+++ libs/sudoku-web/sudoku-logic/src/lib/sudoku-logic/sudoku-multiplayer.service.ts
@@ -0,0 +1,51 @@
+import { Socket, io } from 'socket.io-client';
+
+import { BehaviorSubject } from 'rxjs';
+import { SudokuCell } from '@sudoku-app-game/sudoku-models';
+import { Injectable } from '@angular/core';
+
+export type UpdateBoardMessage = {
+  x: number;
+  y: number;
+  value: number;
+};
+
+@Injectable({ providedIn: 'root' })
+export class SudokuMultiplayer {
+  private socket: Socket | null = null;
+  initBoard$ = new BehaviorSubject<SudokuCell[][] | null>(null);
+
+  constructor() {
+    return;
+  }
+
+  public init(roomId: string, board: SudokuCell[][]) {
+    this.socket = io('http://localhost:3333', { query: { roomId } });
+    this.socket.on('connect', () => {
+      this.emitBoardInit(board);
+    });
+
+    this.socket.on('init-board', (board: SudokuCell[][]) => {
+      this.initBoard$.next(board);
+    });
+  }
+
+  isConnected() {
+    return this.socket?.connected;
+  }
+
+  emitUpdateBoard(msg: UpdateBoardMessage) {
+    if (!this.socket) return;
+    this.socket.emit('update-board', msg);
+  }
+
+  emitBoardInit(board: SudokuCell[][]) {
+    if (!this.socket) return;
+    this.socket.emit('init-board', board);
+  }
+
+  emitNewBoard(board: SudokuCell[][]) {
+    if (!this.socket) return;
+    this.socket.emit('new-board', board);
+  }
+}
